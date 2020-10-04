61c40fcda632c5057f01d0771d73c9e27305c2fa
Ben <ben@vardyfamily.co.uk>
2020-10-04 17:09:32 +0100

    Only create times file if it does not exist

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 yarn build
 cp static/index.php build/api.php
-touch build/times.json
-chmod 666 build/times.json
+if [[ ! -e build/times.json ]]; then
+    touch build/times.json
+    chmod 666 build/times.json
+fi
